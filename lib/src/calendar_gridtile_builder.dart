f6bbac469e6d57246420e2f15ead546091160684
deadcheat <2797681+deadcheat@users.noreply.github.com>
2018-03-26 23:43:27 +0900

    removed unnecessary line

--- lib/src/calendar_gridtile_builder.dart
+++ lib/src/calendar_gridtile_builder.dart
@@ -21,20 +21,6 @@ class DefaultCalendarGridTileBuilder {
     DateTime firstDayOfMonth,
     DateTime lastDayOfMonth,
   ) {
-    var textStyle = new TextStyle(fontWeight: FontWeight.bold);
-    if (baseDate != null && date.month != baseDate.month) {
-      textStyle = textStyle.apply(color: Colors.grey);
-    } else {
-      switch (date.weekday) {
-        case 7:
-          textStyle = textStyle.apply(color: Colors.red);
-          break;
-        case 6:
-          textStyle = textStyle.apply(color: Colors.blue);
-          break;
-        default:
-      }
-    }
     return new GridTile(
       child: child,
     );
